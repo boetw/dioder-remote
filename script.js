3cbc9fae9de3a5d7f56b471fcc79bfb449a77fc2
Boet Waldbjorn <boetw@me.com>
2016-10-06 23:21:14 -0700

    added package

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var i = 0;
 
 for (var easing in Redoid.easingFunctions){
 	redoid.trigger(function(){
-		console.log(' ' + this);
+		console.log(' ' + this + ' function');
 	}.bind(easing));
 	redoid.delay(1000);
 	redoid.transition(i ++ % 2 === 0 ? '#ff0000' : '#ffffff', 2000, easing);
