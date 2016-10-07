58b5db7abc22dace5137c5d6678c226032edb853
Boet Waldbjorn <boetw@me.com>
2016-10-07 06:43:26 -0700

    button controls

--- script.js
+++ script.js
@@ -5,34 +5,50 @@ var Redoid = require('redoid');
 //17 Red grey
 //22 Green white
 //23 Blue purple
-    // piblaster.setPwm(17, 1 );
-    // piblaster.setPwm(22, 0.2 );
-    // piblaster.setPwm(23, 0 );
+// piblaster.setPwm(17, 1 );
+// piblaster.setPwm(22, 0.2 );
+// piblaster.setPwm(23, 0 );
 
 var redoid = Redoid({
 	color: '#ffffff',
 	colorComponentPins: [4, 17, 18],
 	applyColorCallback: null,
-    loopInterval: 25,
-    defaultEasing: 'easeInOutQuad',
-    idleTimeout: 0,
-    idleCallback: null,
-    idleColor: null,
-    idleColorTransitionDuration: 4000,
-    loopTransition: false
+	loopInterval: 25,
+	defaultEasing: 'easeInOutQuad',
+	idleTimeout: 0,
+	idleCallback: null,
+	idleColor: null,
+	idleColorTransitionDuration: 4000,
+	loopTransition: false
 });
 
 var i = 0;
 
-for (var easing in Redoid.easingFunctions){
-	redoid.trigger(function(){
+for (var easing in Redoid.easingFunctions) {
+	redoid.trigger(function() {
 		console.log(' ' + this + ' function');
 	}.bind(easing));
 	redoid.delay(1000);
-	redoid.transition(i ++ % 2 === 0 ? '#ff0000' : '#ffffff', 2000, easing);
+	redoid.transition(i++ % 2 === 0 ? '#ff0000' : '#ffffff', 2000, easing);
 }
 
-function red () {
+function red() {
 	console.log("red");
-	piblaster.setPwm(17, 1 );
+	piblaster.setPwm(17, 1);
+	piblaster.setPwm(22, 0);
+	piblaster.setPwm(23, 0);
+};
+
+function blue() {
+	console.log("blue");
+	piblaster.setPwm(17, 0);
+	piblaster.setPwm(22, 1);
+	piblaster.setPwm(23, 0);
+};
+
+function green() {
+	console.log("green");
+	piblaster.setPwm(17, 0);
+	piblaster.setPwm(22, 0);
+	piblaster.setPwm(23, 1);
 };
